8a3c0a58c832fe509e9b613ec0ed560b4f0977da
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2017-11-19 15:21:25 +0100

    Fix makefile, package.sh and package.json

--- {{cookiecutter.repository_slug}}/package.sh
+++ {{cookiecutter.repository_slug}}/package.sh
@@ -1,4 +1,5 @@
 BINS="bin/{{cookiecutter.package_name}}"  #:bin/other-script
+LIBS="lib/{{cookiecutter.package_name.replace('shellm-', '').replace('-', '_')}}.sh"
 DEPS=shellm-org/shellm-shellman  #:user2/repo2
 BASH_COMPLETIONS="cmp/{{cookiecutter.package_name}}.completion.bash"
 ZSH_COMPLETIONS="cmp/{{cookiecutter.package_name}}.completion.zsh"
