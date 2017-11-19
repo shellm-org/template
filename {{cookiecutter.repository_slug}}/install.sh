3e08e8b3ee863790bf0267e8dd292d74ed65c286
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2017-11-19 22:12:00 +0100

    Handle completions in install.sh

--- {{cookiecutter.repository_slug}}/install.sh
+++ {{cookiecutter.repository_slug}}/install.sh
@@ -5,7 +5,7 @@ PREFIX="${1:-/usr/local}"
 APP_ROOT="$(dirname "$(readlink -f "$0")")"
 
 echo "Create directories"
-mkdir -vp "${PREFIX}"/{bin,lib/shellm,man/man{1,3}}
+mkdir -vp "${PREFIX}"/{bin,completions/{bash,zsh},lib/shellm,man/man{1,3}}
 
 echo "Install binaries"
 cp -vR "${APP_ROOT}"/bin/* "${PREFIX}"/bin
@@ -16,3 +16,7 @@ cp -vR "${APP_ROOT}"/lib/* "${PREFIX}"/lib/shellm
 echo "Install man pages"
 cp -v "${APP_ROOT}"/man/*.1 "${PREFIX}"/man/man1
 cp -v "${APP_ROOT}"/man/*.3 "${PREFIX}"/man/man3
+
+echo "Install completions"
+cp -vR "${APP_ROOT}"/cmp/*.bash "${PREFIX}"/completions/bash
+cp -vR "${APP_ROOT}"/cmp/*.zsh "${PREFIX}"/completions/zsh
