9a7755bad898eea2dbb5dce0820785c66e131483
Timothée Mazzucotelli <timothee.mazzucotelli@protonmail.com>
2018-07-31 20:15:57 +0200

    Update

--- {{cookiecutter.repository_slug}}/lib/{{cookiecutter.package_name.replace('shellm-', '').replace('-', '_')}}.sh
+++ {{cookiecutter.repository_slug}}/lib/{{cookiecutter.package_name.replace('shellm-', '').replace('-', '_')}}.sh
@@ -1,12 +1,6 @@
-if shellm-ndef; then
-shellm-define "
-  {{cookiecutter.package_name.replace('shellm-', '')}}"
-
 ## \brief {{cookiecutter.repository_description}}
 
 ## \fn {{cookiecutter.package_name.replace('shellm-', '')}}
 {{cookiecutter.package_name.replace('shellm-', '')}}() {
   :  # code
 }
-
-fi  #  __{{cookiecutter.package_name.replace('shellm-', '').replace('-', '_').upper()}}_SH
--- {{cookiecutter.repository_slug}}/package.sh
+++ {{cookiecutter.repository_slug}}/package.sh
@@ -1,5 +1,5 @@
 BINS="bin/{{cookiecutter.package_name}}"  #:bin/other-script
 LIBS="lib/{{cookiecutter.package_name.replace('shellm-', '').replace('-', '_')}}.sh"
-DEPS=shellm-org/shellm-shellman  #:user2/repo2
+DEPS=gitlab.com/shellm/core:gitlab.com/shellm/doc
 BASH_COMPLETIONS="cmp/{{cookiecutter.package_name}}.completion.bash"
 ZSH_COMPLETIONS="cmp/{{cookiecutter.package_name}}.completion.zsh"
