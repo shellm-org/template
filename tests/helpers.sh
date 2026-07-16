4919ba52b6b7d696c79b87f54f1d40493d6521e0
Timothée Mazzucotelli <dev@pawamoy.fr>
2026-07-16 18:42:10 +0200

    wip

--- tests/helpers.sh
+++ tests/helpers.sh
@@ -2,8 +2,8 @@ generate() {
   copier copy -f --trust -r HEAD "$1" "$2" \
     -d testing=true \
     -d project_name="shellm template testing" \
-    -d project_description='Testing this great template' \
+    -d project_description="Testing this great template" \
     -d author_fullname="Timothee Mazzucotelli" \
     -d author_username="pawamoy" \
-    -d author_email="pawamoy@pm.me"
+    -d author_email="dev@pawamoy.fr"
 }
